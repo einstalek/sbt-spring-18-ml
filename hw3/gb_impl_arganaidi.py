5cd738aa1ef0e5b9c92e2484e808f335429c4feb
einstalek <arganaidi@phystech.edu>
2018-05-07 01:20:11 +0300

    правки

--- hw3/gb_impl_arganaidi.py
+++ hw3/gb_impl_arganaidi.py
@@ -1,56 +1,53 @@
 #coding=utf-8
 
-from scipy.optimize import minimize
+from scipy.optimize import minimize, fmin_slsqp
 from sklearn.base import BaseEstimator
 from sklearn.tree import DecisionTreeRegressor
 import numpy as np
 
 
 # Параметрами с которыми вы хотите обучать деревья
-TREE_PARAMS_DICT = {'max_depth': 1}
+TREE_PARAMS_DICT = {'max_depth': 4}
 # Параметр tau (learning_rate) для вашего GB
 TAU = 0.05
 
 
 def loss_function(y, h):
-    return sum(-y * np.log(h) - (1 - y) * np.log(1 - h)) / len(y)
+    return sum(np.log(1 + np.exp(-2*y*h))) / len(y)
 
 
-def der_loss_function(y, h):
-    return (h - y) / (h * (1 - h))
-
+def antigrad(y, h):
+    return 2*y / (1 + np.exp(2*y*h))
 
 class SimpleGB(BaseEstimator):
     def __init__(self, tree_params_dict, iters, tau):
         self.tree_params_dict = tree_params_dict
         self.iters = iters
         self.tau = tau
-        
-    def fit(self, X_data, y_data):
-        self.base_algo = DecisionTreeRegressor(**self.tree_params_dict)
-        self.base_algo.fit(X_data, y_data)
         self.estimators = []
         self.weights = []
+        
+    def fit(self, X_data, y_data):
+        self.base_algo = DecisionTreeRegressor(**self.tree_params_dict, random_state=1)
+        self.base_algo.fit(X_data, y_data)
         curr_pred = self.base_algo.predict(X_data)
+
         for iter_num in range(self.iters):
-            # Нужно посчитать градиент функции потерь
-            grad = der_loss_function(y_data, curr_pred)
-            # Нужно обучить DecisionTreeRegressor предсказывать антиградиент
-            algo = DecisionTreeRegressor(**self.tree_params_dict)
-            algo.fit(X_data, -grad)
+            resid = antigrad(y_data, curr_pred)
+            algo = DecisionTreeRegressor(**self.tree_params_dict, random_state=1)
+            algo.fit(X_data, resid)
             self.estimators.append(algo)
-            # Обновите предсказания в каждой точке
-            # Нужно настроить вес для очередного дерева
+
             weight_tuner = lambda x: loss_function(y_data, curr_pred + x * algo.predict(X_data))
-            # варьируем вес и минимизируем loss function
-            res = minimize(weight_tuner, np.array([0.5,]))
-            self.weights.append(res.x)
-            curr_pred += res.x * algo.predict(X_data)
+            res, *_ = fmin_slsqp(weight_tuner, np.array([0.05,]), bounds=[(-1, 1)], iprint=0)
+
+            self.weights.append(res)
+            curr_pred += self.tau * algo.predict(X_data)
         return self
     
     def predict(self, X_data):
         # Предсказание на данных
         res = self.base_algo.predict(X_data)
         for estimator, weight in zip(self.estimators, self.weights):
-            res += weight * estimator.predict(X_data)
-        return res > 0.
+            res += self.tau * estimator.predict(X_data)
+        return np.array([1 if x else -1 for x in res > 0.])
--- hw3/gb_impl_checker.py
+++ hw3/gb_impl_checker.py
@@ -1,13 +1,14 @@
 # coding=utf-8
 
 from sklearn.model_selection import cross_val_score
+from sklearn.metrics import accuracy_score
 import numpy as np
 import os
-import sys
-import imp
+from gb_impl_arganaidi import SimpleGB
 import signal
 import pandas
 import traceback
+import matplotlib.pyplot as plt
 
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
@@ -23,6 +24,7 @@ class Checker(object):
         target = 'left'
         features = [c for c in df if c != target]
         self.target = np.array(df[target])
+        self.target = np.array([-1 if x == 0 else 1 for x in self.target])
         self.data = np.array(df[features])
         self.application = 0
 
@@ -31,14 +33,12 @@ class Checker(object):
             signal.signal(signal.SIGALRM, signal_handler)
             # Time limit на эту задачу 1 минута
             signal.alarm(60)
-            gb_impl = imp.load_source('gb_impl_{}'.format(self.application), script_path)
             self.application += 1
-            # Обучаться будет на 100 итерациях, чтобы одинаковый масштаб был
-            algo = gb_impl.SimpleGB(
-                tree_params_dict=gb_impl.TREE_PARAMS_DICT,
-                iters=100,
-                tau=gb_impl.TAU
-            )
+            algo = SimpleGB(
+                tree_params_dict={'max_depth': 2},
+                iters=50,
+                tau=0.05
+            ).fit(self.data, self.target)
             return np.mean(cross_val_score(algo, self.data, self.target, cv=3, scoring='accuracy'))
         except:
             traceback.print_exc()
